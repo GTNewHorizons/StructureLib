da0e28eb631205cf6f7b1c8df6a1f15d131dbb6f
Glease <4586901+Glease@users.noreply.github.com>
2022-08-04 12:26:04 +0800

    change ISurvivalConstructable contract

--- src/main/java/com/gtnewhorizon/structurelib/alignment/constructable/ISurvivalConstructable.java
+++ src/main/java/com/gtnewhorizon/structurelib/alignment/constructable/ISurvivalConstructable.java
@@ -9,6 +9,8 @@ public interface ISurvivalConstructable extends IConstructable {
     /**
      * Construct the structure using {@link com.gtnewhorizon.structurelib.structure.IStructureElement#survivalPlaceBlock(Object, World, int, int, int, ItemStack, IItemSource, EntityPlayerMP, java.util.function.Consumer)}
      *
+     * @param elementBudget The server configured element budget. You can tune this up a bit if the structure is too big, but
+     *                      generally should not be a 4 digits number to not overwhelm the server
      * @return -1 if done, otherwise number of elements placed this round
      */
     int survivalConstruct(ItemStack stackSize, int elementBudget, IItemSource source, EntityPlayerMP actor);
