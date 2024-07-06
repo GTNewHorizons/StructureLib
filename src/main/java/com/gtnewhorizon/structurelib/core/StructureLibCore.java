6867ed50e62855e7d2a307ae50c947dab41b5e7b
Jason Mitchell <mitchej@gmail.com>
2024-07-06 12:19:16 -0700

    Add mixins

---
+++ src/main/java/com/gtnewhorizon/structurelib/core/StructureLibCore.java
@@ -0,0 +1,53 @@
+package com.gtnewhorizon.structurelib.core;
+
+import java.util.List;
+import java.util.Map;
+import java.util.Set;
+
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
+import com.gtnewhorizon.gtnhmixins.IEarlyMixinLoader;
+import com.gtnewhorizon.structurelib.mixins.Mixins;
+
+import cpw.mods.fml.relauncher.IFMLLoadingPlugin;
+
+public class StructureLibCore implements IFMLLoadingPlugin, IEarlyMixinLoader {
+
+    public static final Logger log = LogManager.getLogger("StructureLibCore");
+
+    @Override
+    public String getMixinConfig() {
+        return "mixins.structurelib.early.json";
+    }
+
+    @Override
+    public List<String> getMixins(Set<String> loadedCoreMods) {
+        return Mixins.getEarlyMixins(loadedCoreMods);
+    }
+
+    @Override
+    public String[] getASMTransformerClass() {
+        return new String[0];
+    }
+
+    @Override
+    public String getModContainerClass() {
+        return null;
+    }
+
+    @Override
+    public String getSetupClass() {
+        return null;
+    }
+
+    @Override
+    public void injectData(Map<String, Object> data) {
+
+    }
+
+    @Override
+    public String getAccessTransformerClass() {
+        return null;
+    }
+}
