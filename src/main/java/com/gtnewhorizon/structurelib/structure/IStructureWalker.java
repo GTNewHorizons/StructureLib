508efcbd0c347872db7e7e1b8113067d6e4aa60f
RecursivePineapple <recursive_pineapple@proton.me>
2025-10-27 23:00:00 -0400

    Swap to double star comments

--- src/main/java/com/gtnewhorizon/structurelib/structure/IStructureWalker.java
+++ src/main/java/com/gtnewhorizon/structurelib/structure/IStructureWalker.java
@@ -2,21 +2,25 @@ package com.gtnewhorizon.structurelib.structure;
 
 import net.minecraft.world.World;
 
-/// A functional interface that is used to iterate over an arbitrary structure.
+/**
+ * A functional interface that is used to iterate over an arbitrary structure.
+ */
 @FunctionalInterface
 public interface IStructureWalker<T> {
 
-    /// Called once per structure element.
-    ///
-    /// @param element The current element
-    /// @param world The current world
-    /// @param x The absolute X position of the element in world coordinates
-    /// @param y The absolute Y position of the element in world coordinates
-    /// @param z The absolute Z position of the element in world coordinates
-    /// @param a The relative A position of the element in structure-local coordinates (pre transform)
-    /// @param b The relative B position of the element in structure-local coordinates (pre transform)
-    /// @param c The relative C position of the element in structure-local coordinates (pre transform)
-    /// @return False to stop iterating, true to continue.
+    /**
+     * Called once per structure element.
+     *
+     * @param element The current element
+     * @param world   The current world
+     * @param x       The absolute X position of the element in world coordinates
+     * @param y       The absolute Y position of the element in world coordinates
+     * @param z       The absolute Z position of the element in world coordinates
+     * @param a       The relative A position of the element in structure-local coordinates (pre transform)
+     * @param b       The relative B position of the element in structure-local coordinates (pre transform)
+     * @param c       The relative C position of the element in structure-local coordinates (pre transform)
+     * @return False to stop iterating, true to continue.
+     */
     boolean visit(IStructureElement<T> element, World world, int x, int y, int z, int a, int b, int c);
 
     default boolean blockNotLoaded(IStructureElement<T> element, World world, int x, int y, int z, int a, int b,
