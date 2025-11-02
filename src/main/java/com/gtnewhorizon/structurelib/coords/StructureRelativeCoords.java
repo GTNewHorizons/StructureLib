1166c651d2942664b4902d61781cd51c0d8aca8b
RecursivePineapple <recursive_pineapple@proton.me>
2025-11-02 14:32:16 -0500

    Add coordinate system tests

--- src/main/java/com/gtnewhorizon/structurelib/coords/StructureRelativeCoords.java
+++ src/main/java/com/gtnewhorizon/structurelib/coords/StructureRelativeCoords.java
@@ -3,8 +3,8 @@ package com.gtnewhorizon.structurelib.coords;
 import com.gtnewhorizon.structurelib.alignment.IntegerAxisSwap;
 
 /**
- * Coordinates relative to the structure. 0,0,0 is the controller. 1,0,0 is the block immediately behind the controller
- * (looking at it from its face). 0,0,1 is the block to the right of the controller. 0,1,0 is the block below the
+ * Coordinates relative to the structure. 0,0,0 is the controller. 0,0,1 is the block immediately behind the controller
+ * (looking at it from its face). 1,0,0 is the block to the right of the controller. 0,1,0 is the block below the
  * controller.
  */
 public class StructureRelativeCoords implements CoordinateSystem<StructureRelativeCoords, ControllerRelativeCoords> {
