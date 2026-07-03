f4c14fb8b18152008a08f6c3d532c3f02bd67ca6
AnsonYeung <nanolify@gmail.com>
2026-07-03 23:14:14 +0800

    Read channel in getBlocksToPlace

--- src/main/java/com/gtnewhorizon/structurelib/structure/StructureUtility.java
+++ src/main/java/com/gtnewhorizon/structurelib/structure/StructureUtility.java
@@ -3015,7 +3015,7 @@ public class StructureUtility {
             @Override
             public BlocksToPlace getBlocksToPlace(T t, World world, int x, int y, int z, ItemStack trigger,
                     AutoPlaceEnvironment env) {
-                return backing.getBlocksToPlace(t, world, x, y, z, trigger, env);
+                return backing.getBlocksToPlace(t, world, x, y, z, ChannelDataAccessor.withChannel(trigger, channel), env);
             }
 
             @Deprecated
