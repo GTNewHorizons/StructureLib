c2c5ae397c7569f9b7fae96d1cfb8bc26a1fa0ea
Alex Vermette <vermz@hotmail.ca>
2026-09-16 23:02:46 -0400

    fix: commited dead code by accient, removed

--- src/main/java/com/gtnewhorizon/structurelib/mixins/early/MixinGuiContainer_PickChannel.java
+++ src/main/java/com/gtnewhorizon/structurelib/mixins/early/MixinGuiContainer_PickChannel.java
@@ -59,19 +59,6 @@ public abstract class MixinGuiContainer_PickChannel {
                 Keyboard.isKeyDown(Minecraft.getMinecraft().gameSettings.keyBindSneak.getKeyCode()));
     }
 
-    /**
-     * This mixin is to avoid the shift-to-dump-all NEI feature; it triggers on all mouse keys.
-     */
-    // @Inject(method = "mouseMovedOrUp(III)V", at = @At("HEAD"), cancellable = true)
-    private void slib$onMouseMovedOrUp(int mouseX, int mouseY, int state, CallbackInfo ci) {
-        if (state != 2) return;
-        EntityPlayer player = Minecraft.getMinecraft().thePlayer;
-        if (player == null) return;
-        ItemStack cursorStack = player.inventory.getItemStack();
-        if (cursorStack == null || !(cursorStack.getItem() instanceof ItemConstructableTrigger)) return;
-        ci.cancel();
-    }
-
     @Invoker("getSlotAtPosition")
     protected abstract Slot slib$invokeGetSlotAtPosition(int x, int y);
 }
